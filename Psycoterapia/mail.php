de4582dd19e9458d5ca2056e05801d08e0dabf78
Erik Robles <30667777+ErikRobles@users.noreply.github.com>
2017-11-17 13:01:36 -0600

    Add files via upload

---
+++ Psycoterapia/mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "ambriz57@msn.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Gracias por contactarnos! Estaremos en contacto en menos de 48 hrs. ";
+?>
