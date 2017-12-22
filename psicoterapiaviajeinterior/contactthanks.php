808b4c26a739d52810eb8f17cc377f13cd0ea481
Erik Robles <30667777+ErikRobles@users.noreply.github.com>
2017-12-21 21:39:46 -0600

    Add files via upload

---
+++ psicoterapiaviajeinterior/contactthanks.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+<style>
+div {
+  background: blue;
+  padding: 30px;
+}
+h1 {
+  color: white;
+  font-weight: bold;
+}
+a {
+  text-decoration: none;
+  font-size: 1.25em;
+  color: blue;
+  font-weight: bold;
+  padding: 15px;
+}
+</style>
+<div>
+  <h1>¡Gracias por contactarme, <br>En las próximas 48 horas estaré respondiendo tu mensaje!</h1>
+  <button><a href="index.html">INICIO</a></button>
+</div>
--- psicoterapiaviajeinterior/mail.php
+++ psicoterapiaviajeinterior/mail.php
@@ -1,17 +1,16 @@
 <?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$comments = $_POST['comments'];
+$formcontent="De: $name \n Telefono: $phone \n Mensaje: $comments";
+$recipient = "psicoter@psicoterapiaviajeinterior.com";
+$subject = "Formulario de Contacto de tu sitio web";
+$mailheader = "De: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("¡Error!");
 
+//echo "<h1>¡Gracias por contactarme, <br>En las próximas 48 horas estaré respondiendo tu mensaje!</h1>";
 
-
-$from="psicoterapiaviajeinterior@gmail.com";
-$name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$subject=$_POST['subject'];
-$comments=$_POST['comments'];
-
-mail($name, $email, $phone, $subject, $comments, "from:".$from);
-
-echo "Gracias por contactarnos! Estaremos en contacto en menos de 48 hrs."
-
-
+header("Location: contactthanks.php");
+exit;
 ?>
